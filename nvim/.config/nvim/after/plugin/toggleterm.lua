759ceffb8b541a46ade352269d4c0d39b4501c8e
cluttrdev <andreas@cluttr.tech>
2023-06-05 07:21:10 +0200

    nvim: Change toggleterm open mapping

--- nvim/.config/nvim/after/plugin/toggleterm.lua
+++ nvim/.config/nvim/after/plugin/toggleterm.lua
@@ -1,11 +1,11 @@
 require("toggleterm").setup({
     size = 20,
-    open_mapping = [[<C-SPACE>]],
+    open_mapping = [[<M-Space>]],
 })
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<C-c>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
